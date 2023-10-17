b4766d0c99b776f593566a03d18287eeeb9a073a
JustFiesta <mateusz.bocak9@gmail.com>
2023-10-18 00:41:27 +0200

    basic sketch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./blog-site/src/**/*.{html,js}"],
+  content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'esm': '0.3rem'
+      },
+      fontFamily: {
+        'lato': ["'Lato'", ],
+      },
+      colors: {
+      'grad-t': '#263854',
+      'grad-b': '#6fb5c2',
+      'light-orange': '#FF9F64'
+      }
+    },
+    
   },
   plugins: [],
 }
